779fd5a6e294e83a7ad90732d52fd3894fd773bd
yizhongw <eastonwyz@gmail.com>
2024-09-26 05:38:01 +0000

    More elegant way for bbh

--- scripts/regen_bon_data.sh
+++ scripts/regen_bon_data.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
-set -x
+# set -x
 
 # set variables
 GENERATOR="allenai/tulu-2-13b"
-N_COMPLETIONS=16
+N_COMPLETIONS=1
 TEMPERATURE=0.7
 TOP_P=1.0
-GPU_COUNT=2
+GPU_COUNT=4
 BATCH_SIZE_VLLM=1000
-PER_TASK_MAX_EXAMPLES=1000
+PER_TASK_MAX_EXAMPLES=100
 EXAMPLE_SAMPLING_SEED=42
 MODEL_MAX_LENGTH=8192
 MAX_GEN_TOKS=4096
-OUTPUT_DIR="output/oe-eval-bon-candidates-new"
+OUTPUT_DIR="output/oe-eval-bon-candidates"
 SUBDIR="${GENERATOR##*/}-n-${N_COMPLETIONS}"
 UPLOAD_TO_HF=true
 HF_REPO_ID="ai2-adapt-dev/oe-eval-bon-candidates"
 HF_PATH_IN_REPO="${GENERATOR##*/}-n-${N_COMPLETIONS}"
 
-TASKS=(
-    'gsm8k'
-    'codex_humaneval'
-    'codex_humanevalplus'
-    'ifeval'
-    'popqa'
-    'alpaca_eval_v1'
-    'alpaca_eval_v2'
-    'bbh:cot::none'  # This is a suite of tasks. Run this sepeartely with PER_TASK_MAX_EXAMPLES=50.
-)
 
 GENERATION_KWARGS="{
     \"repeats\": 1,
@@ -38,19 +28,126 @@ GENERATION_KWARGS="{
     \"truncate_context\": true
 }"
 
+declare -A TASK_SPECS=(
+    ["gsm8k"]="{
+        \"task_name\": \"gsm8k\",
+        \"split\": \"test\",
+        \"primary_metric\": \"exact_match\",
+        \"use_chat_format\": true,
+        \"num_shots\": 8,
+        \"fewshot_source\": \"STD:GSM8k\",
+        \"chat_overrides\": {
+            \"context_kwargs\": {
+                \"fewshot_as_multiturn\": false
+            }
+        },
+        \"generation_kwargs\": ${GENERATION_KWARGS}
+    }"
+    ["codex_humaneval"]="{
+        \"task_name\": \"codex_humaneval\",
+        \"primary_metric\": \"pass_at_1\",
+        \"use_chat_format\": true,
+        \"metric_kwargs\": {
+            \"pass_at_ks\": [1]
+        },
+        \"generation_kwargs\": ${GENERATION_KWARGS}
+    }"
+    ["codex_humanevalplus"]="{
+        \"task_name\": \"codex_humanevalplus\",
+        \"primary_metric\": \"pass_at_1\",
+        \"use_chat_format\": true,
+        \"metric_kwargs\": {
+            \"pass_at_ks\": [1]
+        },
+        \"generation_kwargs\": ${GENERATION_KWARGS}
+    }"
+    ["ifeval"]="{
+        \"task_name\": \"ifeval\",
+        \"primary_metric\": \"prompt_level_loose_acc\",
+        \"split\": \"train\",
+        \"use_chat_format\": true,
+        \"metric_kwargs\": {
+            \"aggregation_levels\": [\"prompt\", \"inst\"],
+            \"strictness_levels\": [\"strict\", \"loose\"],
+            \"output_individual_metrics\": true
+        },
+        \"generation_kwargs\": ${GENERATION_KWARGS}
+    }"
+    ["popqa"]="{
+        \"task_name\": \"popqa\",
+        \"use_chat_format\": true,
+        \"chat_overrides\": {
+            \"context_kwargs\": {
+                \"fewshot_as_multiturn\": false
+            }
+        },
+        \"generation_kwargs\": ${GENERATION_KWARGS}
+    }"
+    ["alpaca_eval_v1"]="{
+        \"task_name\": \"alpaca_eval\",
+        \"primary_metric\": \"win_rate\",
+        \"metric_kwargs\": {
+            \"alpaca_eval_version\": 1
+        },
+        \"use_chat_format\": true,
+        \"generation_kwargs\": ${GENERATION_KWARGS}
+    }"
+    ["alpaca_eval_v2"]="{
+        \"task_name\": \"alpaca_eval\",
+        \"primary_metric\": \"length_controlled_winrate\",
+        \"metric_kwargs\": {
+            \"alpaca_eval_version\": 2
+        },
+        \"use_chat_format\": true,
+        \"generation_kwargs\": ${GENERATION_KWARGS}
+    }"
+    ["bbh"]="{
+        \"task_name\": \"bbh:cot::tulu\",
+        \"primary_metric\": \"exact_match\",
+        \"split\": \"test\",
+        \"use_chat_format\": true,
+        \"num_shots\": 3,
+        \"chat_overrides\": {
+            \"context_kwargs\": {
+                \"fewshot_as_multiturn\": false
+            }
+        },
+        \"generation_kwargs\": ${GENERATION_KWARGS}
+    }"
+)
+
+
+TASKS=(
+    'gsm8k'
+    'codex_humaneval'
+    'codex_humanevalplus'
+    'ifeval'
+    'popqa'
+    'alpaca_eval_v1'
+    'alpaca_eval_v2'
+    'bbh'  # This is a suite of tasks. Run this sepeartely with PER_TASK_MAX_EXAMPLES=50.
+)
+
+
+
 # Here for each task, we run oe-eval N_COMPLETIONS times to get N_COMPLETIONS generations for each task.
 # We do this outside oe-eval because in this way we can get the score for each generation.
 # These scores will be used in bon evaluation, without rerunning the oe-eval.
 for TASK_NAME in "${TASKS[@]}"; do
-    for i in $(seq 3 3); do
+    TASK_SPEC_ARGS=${TASK_SPECS[${TASK_NAME}]}
+    ACTUAL_TASK_NAME=$(echo "${TASK_SPEC_ARGS}" | jq -r '.task_name')
+    TASK_SPEC_ARGS=$(echo "${TASK_SPEC_ARGS}" | jq 'del(.task_name)')
+
+    for i in $(seq 1 ${N_COMPLETIONS}); do
         echo "Running oe-eval for task ${TASK_NAME} for the ${i}th completion."
+        echo "Task spec args: ${TASK_SPEC_ARGS}"
         # Randomly generate a seed to initialize VLLM
         # Otherwise, oe-eval will use the default seed 1234, which causes the same generations for every run.
         VLLM_INIT_SEED=${i}
+        echo "VLLM init seed: ${VLLM_INIT_SEED}"
         oe-eval \
-            --task "${TASK_NAME}"  \
-            --task-args "{\"generation_kwargs\":${GENERATION_KWARGS}}" \
-            --use-chat-format true \
+            --task "${ACTUAL_TASK_NAME}"  \
+            --task-args "${TASK_SPEC_ARGS}" \
             --model ${GENERATOR} \
             --model-args "{\"model_path\":\"${GENERATOR}\", \"seed\":${VLLM_INIT_SEED}, \"max_length\":${MODEL_MAX_LENGTH}}" \
             --model-type vllm \
